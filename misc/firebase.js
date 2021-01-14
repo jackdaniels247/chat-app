d761f72434ae430897094049c4cf8316afff37c0
jackdaniels247 <64647294+jackdaniels247@users.noreply.github.com>
2021-01-14 19:49:39 +0530

    attached firebase

---
+++ misc/firebase.js
@@ -0,0 +1,14 @@
+
+import firebase from 'firebase/app'
+
+
+const config={
+    apiKey: "AIzaSyDQwu5xUvKVkupe8wwflYH2xBuKcUzAuO8",
+    authDomain: "chat-web-app-4b958.firebaseapp.com",
+    projectId: "chat-web-app-4b958",
+    storageBucket: "chat-web-app-4b958.appspot.com",
+    messagingSenderId: "820885800593",
+    appId: "1:820885800593:web:9aeb9815a5728aa5a86130"
+  };
+
+const app=  firebase.initializeApp(config);
