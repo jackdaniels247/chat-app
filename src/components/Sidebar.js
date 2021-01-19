88de7b9be819807deac670e866f6602a33751c1c
jackdaniels247 <64647294+jackdaniels247@users.noreply.github.com>
2021-01-19 21:30:59 +0530

    add button to create new chat room

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import CreateChatroomBtn from './CreateChatroomBtn';
 import DashboardToggle from './dashboard/DashboardToggle';
 
 const Sidebar = () => 
@@ -6,6 +7,7 @@ const Sidebar = () =>
         <div className='h-100 pt-2'>
 <div>
     <DashboardToggle />
+    <CreateChatroomBtn/>
 </div>
 bottom
         </div>
